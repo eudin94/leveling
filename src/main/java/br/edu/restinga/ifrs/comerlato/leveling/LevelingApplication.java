2b58742353d0ae14adfd6e767416081c77f50b98
eduardomc94 <eduardomc94@gmail.com>
2023-03-15 22:01:51 -0300

    initial commit

---
+++ src/main/java/br/edu/restinga/ifrs/comerlato/leveling/LevelingApplication.java
@@ -0,0 +1,13 @@
+package br.edu.restinga.ifrs.comerlato.leveling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LevelingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/restinga/ifrs/comerlato/leveling/LevelingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.restinga.ifrs.comerlato.leveling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
