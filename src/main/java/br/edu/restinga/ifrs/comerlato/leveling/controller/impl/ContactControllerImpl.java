49ae8b486b9866ddb0d9691ce354ad32d51ae1e9
Eduardo Comerlato <eduardo.comerlato@southsystem.com.br>
2023-03-22 09:31:57 -0300

    Improving swagger config

--- src/main/java/br/edu/restinga/ifrs/comerlato/leveling/controller/impl/ContactControllerImpl.java
+++ src/main/java/br/edu/restinga/ifrs/comerlato/leveling/controller/impl/ContactControllerImpl.java
@@ -3,14 +3,12 @@ package br.edu.restinga.ifrs.comerlato.leveling.controller.impl;
 import br.edu.restinga.ifrs.comerlato.leveling.controller.ContactController;
 import br.edu.restinga.ifrs.comerlato.leveling.dto.ContactDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("contact-api")
-@Tag(name = "API de Contatos", description = "API ara cadastro e listagem de contatos")
+@Tag(name = "Contact Controller", description = "Register and find contacts")
 public class ContactControllerImpl implements ContactController {
 
     @Override
